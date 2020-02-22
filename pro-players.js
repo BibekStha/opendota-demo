11adb5670a5ad2a9de1375bebd14dda629061518
Bibek Shretha <bibekshrestha@Bibeks-Work-Tool.local>
2020-02-22 12:21:54 -0500

    add navigation and basic pro players page

---
+++ pro-players.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Run this block of code when html is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  let proPlayersEl = document.getElementById("pro-players");
+
+  fetch("https://api.opendota.com/api/proPlayers")
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      data.forEach(player => {
+        let name = player.personaname;
+        let imageUrl = player.avatarfull;
+
+        console.log(name);
+        console.log(imageUrl);
+
+        let playerEl = document.createElement("div");
+        playerEl.setAttribute("class", "col col-2 p-2");
+
+        let imageEl = document.createElement("img");
+        imageEl.setAttribute("src", imageUrl);
+
+        let nameEl = document.createElement("div");
+        nameEl.setAttribute("class", "text-center");
+        nameEl.innerHTML = name;
+
+        playerEl.appendChild(imageEl);
+        playerEl.appendChild(nameEl);
+
+        proPlayersEl.appendChild(playerEl);
+      });
+    });
+});
